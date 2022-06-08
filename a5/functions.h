228e2d978f332d5a31c05063a5cffdcca4279f95
zanphua <72734607+zanphua@users.noreply.github.com>
2022-06-08 23:27:15 +0800

    a5 done

--- a5/functions.h
+++ a5/functions.h
@@ -1,3 +1,14 @@
+/*!*****************************************************************************
+\file functions.h
+\author Zandra Phua Si-Yu
+\par DP email: p.siyuzandra@digipen.edu
+\par Course: CSD3182
+\par Section: B
+\par Assignment 5 (Bellman-Ford's Search)
+\date 08-06-2021
+\brief
+This file has declarations and definitions that are required for submission
+*******************************************************************************/
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 
@@ -6,6 +17,7 @@
 #include <array>
 #include <climits>
 #include <algorithm>
+#include <string>
 
 #include "data.h"
 
@@ -32,39 +44,76 @@ namespace AI
         BellmanFord(int* matrix = nullptr)
             : matrix{ matrix }, distance{ nullptr }, predecessor{ nullptr }
         {
+            distance = new int[SIZE];
+            predecessor = new int[SIZE];
 
-            //...
-
+            //*distance = SIZE;
+            //*predecessor = SIZE;
          }
 
         ~BellmanFord()
         {
-
-            //...
- 
+            delete distance; 
+            delete predecessor;
         }
 
         // Runs the algorithm. Returns false when negative 
         // cycles are found, and true otherwise.
         bool run(int starting = 0)
         {
-            UNUSED(starting)
+            int temp = SIZE;
+            if (temp <= 0) return false;
 
-            //...
- 
-            return true;
+            for (int i{ 0 }; i < SIZE; ++i)
+            {
+                // Initialize the distance to all nodes to be infinity
+                distance[i] = inf;
+                // Initialize predecessor array which will be used in shortest path
+                // reconstruction after the algorithm has terminated.
+                predecessor[i] = null;
+            }
+
+            // except for the start node which is zero.
+            distance[starting] = 0;
+
+            // For each node, apply relaxation for all the edges
+            for (int k{ 0 }; k < SIZE; ++k)
+            {
+                int counter = 0; // Number of relaxations
+                for (int j = 0; j < SIZE; ++j)
+                    for (int i = 0; i < SIZE; ++i)
+                    {
+                        if ((j != i) && (distance[j] != inf) && 
+                            (matrix[j * SIZE + i] != inf))
+                        {
+                            int new_dist = distance[j] + matrix[j * SIZE + i];
+                            if (new_dist < distance[i])
+                            {
+                                // Relaxation
+                                distance[i] = new_dist;
+                                predecessor[i] = j;
+                                counter++;
+                            }
+                        }
+                    }
+                if (counter == 0) // Stop when no more relaxation
+                    return true;  // There is no negative cycles
+            }
+
+            return false; // There is a negative cycle
         }
 
         // Interpretation of the result that reconstructs the shortest path
         // from starting point to target
         std::vector<int> getPath(int target)
         {
-            UNUSED(target)
-
             std::vector<int> path{};
+            for (int i = target; predecessor[i] != null; i = predecessor[i])
+            {
+                path.emplace_back(i);
+            }
 
-            //...
- 
+            std::reverse(path.begin(), path.end());
             return path;
         }
 
@@ -72,21 +121,42 @@ namespace AI
         // description) of the shortest path from start to end with cost
         std::vector<std::array<int, 3>> getRoute(int target)
         {
-            UNUSED(target)
-
             std::vector<std::array<int, 3>> route{};
 
-            //...
+            for (int i = target; predecessor[i] != null; i = predecessor[i])
+            {
+                std::array<int, 3> arr1 {predecessor[i], i, distance[i]};
+                route.emplace_back(arr1);
+            }
 
+            for (size_t i = 0; i < route.size() - 1; ++i)
+            {
+                route[i][2] -= route[i + 1][2];
+            }
+
+            std::reverse(route.begin(), route.end());
             return route;
         }
 
         friend std::ostream& operator<<(std::ostream& os, const BellmanFord& rhs)
         {
+            std::string s{ "[" };
 
-            UNUSED(rhs)
- 
-            //...
+            for (int i{ 0 }; i < SIZE; ++i)
+            {
+                s += (rhs.distance[i] == inf ? "inf" : std::to_string(rhs.distance[i])) + (i < SIZE - 1 ? "," : "");
+            }
+
+
+            s += "] [";
+
+
+            for (int i{ 0 }; i < SIZE; ++i)
+            {
+                s += (rhs.predecessor[i] == -1 ? "null" : std::to_string(rhs.predecessor[i])) + (i < SIZE - 1 ? "," : "");
+            }
+            s += "]";
+            os << s;
 
             return os;
         }
